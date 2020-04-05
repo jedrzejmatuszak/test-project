21cc11e587dfbec3fcaf16e8dde72226a1864800
Jędrzej <jedrzej.matuszak@gmail.com>
2020-04-05 18:54:32 +0200

    test

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-from .mx_objects import BatchMonitor
+from mx_objects import BatchMonitor
 
 
 class Config:
@@ -6,9 +6,20 @@ class Config:
     def __init__(self, murex, env):
         self.murex = murex
         self.env = env
-        self.batch_moniotr = None
+        self.batch_monitor = None
         self.jcl = None
 
     def add_batch_monitor(self, application_exec):
-        obj = BatchMonitor(application_exec)
-        self.batch_moniotr = obj
+        if '=' in application_exec:
+            obj = BatchMonitor._create_batch_monitor(application_exec)
+        else:
+            obj = BatchMonitor(application_exec)
+        self.batch_monitor = obj
+
+
+command = {
+    'BM_TASK1_NEW': '/murex/uimx/bin/ProcessingScript.sh G=FO U=MXEODUSER N=DUPAJASIA',
+    'BM_TASK2_OLD': '/murex/uimx/bin/ProcessinsScript.sh BM:TASK2_DUPA SYSMO_ALL MUREX.NO_FLEX',
+    'BM_TASK3_OLD': '/bin/ProcessingScript.sh BM:TASK3_DUPA IF_FUN_GL MUREX_NO_FLEX',
+    'BM_TASK4_NEW': '/env/bin/PS.sh script G=BO D=FO_AT N=MUREXEODUSER'
+}
--- mx_objects.py
+++ mx_objects.py
@@ -1,6 +1,32 @@
+import re
+
+
 class BatchMonitor:
 
-    def __init__(self, application_exec, ps=None, task=None):
+    re_new_ps = re.compile('(?<=PS=)[\S]+', flags=re.ASCII)
+    re_old_ps = re.compile('(?<=ProcessingScript[.]sh)[\S]+', flags=re.ASCII)
+    re_mxgroup = re.compile('(?<=G=)[\S]+', flags=re.ASCII)
+    re_new_nickname = re.compile('(?<=N=)[\S]+', flags=re.ASCII)
+    re_old_nickname = re.compile('(?<=MXPR)[\S]+', flags=re.ASCII)
+    re_desk = re.compile('(?<=D=)[\S]+', flags=re.ASCII)
+    re_user = re.compile('(?<=U=)[\S]+', flags=re.ASCII)
+
+    def __init__(self, application_exec):
         self.application_exec = application_exec
-        self.ps = ps
-        self.task = task
+        self.ps = re.match(self.re_old_ps, application_exec).group()
+        self.nickname = re.match(self.re_old_nickname, application_exec).group()
+
+    @classmethod
+    def _create_batch_monitor(cls, application_exec):
+        cls.application_exec = application_exec
+        cls.ps, cls.mxgroup, cls.nickname, cls.desk, cls.user = cls._get_application_execution_params(application_exec)
+        return cls
+
+    @classmethod
+    def _get_application_execution_params(cls, application_exec):
+        ps = re.match(cls.re_new_ps, application_exec).group() if re.match(cls.re_new_ps, application_exec) is not None else None
+        mxgroup = re.match(cls.re_mxgroup, application_exec).group() if re.match(cls.re_mxgroup, application_exec) is not None else None
+        nickname = re.match(cls.re_new_nickname, application_exec).group() if re.match(cls.re_new_nickname, application_exec) is not None else None
+        desk = re.match(cls.re_desk, application_exec).group() if re.match(cls.re_desk, application_exec) is not None else None
+        user = re.match(cls.re_user, application_exec).group() if re.match(cls.re_user, application_exec) is not None else None
+        return ps, mxgroup, nickname, desk, user
